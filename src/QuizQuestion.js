00f5e22cd42408386c0921ebe740c631651d06b4
hannah.west <hannah.west@cerner.com>
2018-07-26 12:27:27 -0500

    eighth

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,11 +5,11 @@ class QuizQuestion extends Component {
     return (
     <main>
       <section>
-        <p> {this.props.quiz_question.instruction_text}</p>
+        <p>{this.props.quiz_question.instruction_text}</p>
       </section>
       <section className="buttons">
         <ul>
-          <li>{this.props.quiz_question.answer_options[0]}</li>
+          <li>{this.props.quiz_question[0].answer_options}</li>
         </ul>
       </section>
     </main>
