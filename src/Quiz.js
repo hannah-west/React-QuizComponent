981fc2b6efeac37a46e99c784d72f492ad5b9597
hannah.west <hannah.west@cerner.com>
2018-07-26 10:57:53 -0500

    first

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion'
 
  let quizData = require('./quiz_data.json')
 
@@ -9,7 +10,7 @@ import React, { Component } from 'react';
     }
    render() {
      return(
-     <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div> </div>
+     <div><QuizQuesiton prop.quiz_question{quizData.quiz_questions[quiz_position - 1]}/></div>
       )
     }
  }
