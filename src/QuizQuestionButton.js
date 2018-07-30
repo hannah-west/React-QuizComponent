2046a89e4e88c7de59e40bcaf7f82ee5a3a12c2f
hannah.west <hannah.west@cerner.com>
2018-07-30 10:19:00 -0500

    fix

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class QuizQuestionButton extends Component {
     render() {
         return (
-          <li><Button>{this.props.button_text}</Button></li>  
+          <li><button>{this.props.button_text}</button></li>  
         )
     }
 }
